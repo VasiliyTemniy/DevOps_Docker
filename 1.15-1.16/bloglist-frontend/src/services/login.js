44620a256b661a0732c7ec649566038931868e49
VasiliyTemniy <flash_er@mail.ru>
2023-04-14 13:42:54 +0300

    not needed here, sorry

--- 1.15-1.16/bloglist-frontend/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = `${process.env.REACT_APP_BACKEND_URL}/login`
-
-const login = async (credentials) => {
-  const response = await axios.post(baseUrl, credentials)
-  return response.data
-}
-
-export default { login }
